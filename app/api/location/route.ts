063d80350bbddfaf8abe306e03e08b29c2be8d78
Aalok Tanwar <aaloktanwar88@gmail.com>
2024-06-25 10:29:10 +0530

    bug fix

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -4,7 +4,7 @@ import {NextResponse} from "next/server";
 export const GET = async (req: Request) => {
     try {
         const response = await axios.get('http://ip-api.com/json/');
-        return NextResponse.json({ data: response })
+        return NextResponse.json(response.data)
     } catch (error: any) {
         return NextResponse.json({ error: error.message, message: 'Failed to fetch location' })
     }
